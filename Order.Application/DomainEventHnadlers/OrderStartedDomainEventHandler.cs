5d57327702b1efb8a15d6ee10917044336957208
oguzh <oguzh@DESKTOP-E9MDQSC>
2023-07-31 19:16:30 +0300

    Add project files.

---
+++ Order.Application/DomainEventHnadlers/OrderStartedDomainEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Order.Application.Repository;
+using Order.Domain.AggregateModels.BuyerModels;
+using Order.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Application.DomainEventHnadlers
+{
+    public class OrderStartedDomainEventHandler : INotificationHandler<OrderStartedDomainEvent>
+    {
+        private readonly IBuyerRepository _buyerRepository;
+
+        public OrderStartedDomainEventHandler(IBuyerRepository buyerRepository)
+        {
+            _buyerRepository = buyerRepository;
+        }
+         
+        public Task Handle(OrderStartedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            if (notification.Order.UserName == "")
+            {
+                var buyer = new Buyer(notification.UserName);
+                //_buyerRepository.Add(buyer);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
